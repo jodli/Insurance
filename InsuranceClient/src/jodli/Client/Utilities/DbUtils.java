c047b764f638658f740d3766dbe8fd96aa79368d
jodli <job87@web.de>
2014-01-03 13:11:42 +0100

    Added database utilities. Singleton and creates a sqlite queue with database at db/database.

---
+++ InsuranceClient/src/jodli/Client/Utilities/DbUtils.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2013 Jan-Olaf Becker
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package src.jodli.Client.Utilities;
+
+import java.io.File;
+
+import com.almworks.sqlite4java.SQLiteQueue;
+
+public class DbUtils {
+	private File databaseFile;
+	private static final String DB_PATH = "db/database";
+	private SQLiteQueue queue;
+	private static DbUtils instance = null;
+
+	private DbUtils() {
+		databaseFile = new File(DB_PATH);
+
+		queue = new SQLiteQueue(databaseFile);
+		queue.start();
+	}
+
+	public static synchronized DbUtils get() {
+		if (instance == null) {
+			instance = new DbUtils();
+			instance.queue.execute(new initJob());
+		}
+
+		return instance;
+	}
+
+	public void dispose() throws InterruptedException {
+		if (queue != null) {
+			queue.stop(true).join();
+		}
+	}
+}
